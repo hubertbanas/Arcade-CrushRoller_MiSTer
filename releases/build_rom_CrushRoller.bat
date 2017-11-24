291340d21c82a44dc04c28de3d12b84b5467fff6
sorgelig <pour.garbage@gmail.com>
2017-11-25 02:39:33 +0800

    Load ROM from SD

---
+++ releases/build_rom_CrushRoller.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set    zip=crush2.zip
+set ifiles=tp1+tp5a+tp2+tp6+tp3+tp7+tp4+tp8+tp1+tp5a+tp2+tp6+tp3+tp7+tp4+tp8+tpa+tpc+tpb+tpd
+set  ofile=a.crush.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
